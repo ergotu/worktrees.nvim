1b6538bb0c1c090641e826006427c16634528969
Jordi Lambrechts <jordi@intigriti.com>
2025-03-14 13:05:40 +0100

    feat: add temporary init file with helpers for testing

---
+++ lua/worktrees/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.setup(opts)
+  -- Currently no-op, but provides standard entry point
+  -- for future configuration
+end
+
+-- Core module exports for testing access
+M.git = {
+  worktree = require('worktrees.lib.git.worktree'),
+  refs = require('worktrees.lib.git.refs'),
+  git = require('worktrees.lib.git'),
+  branch = require('worktrees.lib.git.branch'),
+}
+M.util = require('worktrees.lib.util')
+M.notification = require('worktrees.lib.notification')
+
+return M
